e8c1b5f837ece1a5876f44a845fd78a2bf7e9072
sadh123 <45258058+sadh123@users.noreply.github.com>
2020-10-02 13:00:43 +0545

    updatedd

---
+++ precache-manifest.0bbe3cac9b8e8830cbdfeaa3c10987af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b058a7dbfc1ef808e5206f9c4e32ec30",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9931b9994df6e04c0527",
+    "url": "./static/css/main.90148a3d.chunk.css"
+  },
+  {
+    "revision": "32af6d25cb3f5fb4e4df",
+    "url": "./static/js/2.2e8284b8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.2e8284b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9931b9994df6e04c0527",
+    "url": "./static/js/main.a46d4f4c.chunk.js"
+  },
+  {
+    "revision": "dbab036f87ca1350243a",
+    "url": "./static/js/runtime-main.851de3a6.js"
+  }
+]);
